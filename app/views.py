ac37882fa67c06a9fd8fdd89687bcd2c5d42fff7
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-26 21:34:28 +0300

    Feat: Adds macro-file and implements macro in templates

--- app/views.py
+++ app/views.py
@@ -6,14 +6,14 @@ from .request import get_movies
 # Views | Index route
 @app.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
 
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title,popular = popular_movies)
 
