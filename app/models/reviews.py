1faffd9c78ccc5ed0d51647fb39bccc119f6447e
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-28 20:10:21 +0300

    All logic to the WTF forms

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -1,3 +1,6 @@
+from urllib import response
+
+
 class Review:
 
     all_reviews = []
@@ -16,3 +19,14 @@ class Review:
     @classmethod
     def clear_reviews(cls):
         Review.all_reviews.clear()
+
+    @classmethod
+    def get_reviews(cls,id):
+
+        response = []
+
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+                
+        return response
